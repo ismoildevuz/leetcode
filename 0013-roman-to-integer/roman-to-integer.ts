c94f6841c0d72889734f8b780d925a6e9c100911
ismoildevuz <ismoildevuz@gmail.com>
2024-09-30 17:20:51 +0500

    #easy 0013-roman-to-integer

--- 0013-roman-to-integer/roman-to-integer.ts
+++ 0013-roman-to-integer/roman-to-integer.ts
@@ -1,34 +1,35 @@
-type Roman = 'I' | 'V' | 'X' | 'L' | 'C' | 'D' | 'M'
+type RomanType = 'I' | 'V' | 'X' | 'L' | 'C' | 'D' | 'M'
 
-function change(current: Roman, next?: Roman): [number, number] {
+const romanToIntegerMap: Record<RomanType, number> = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+}
+
+function change(current: RomanType, next?: RomanType): [number, number] {
     let step = 1
     switch (current) {
         case 'I':
             if (next === 'X') return [++step, 9]
             if (next === 'V') return [++step, 4]
-            return [step, 1]
-
-        case 'V':
-            return [step, 5]
+            return [step, romanToIntegerMap[current]]
 
         case 'X':
             if (next === 'C') return [++step, 90]
             if (next === 'L') return [++step, 40]
-            return [step, 10]
-
-        case 'L':
-            return [step, 50]
+            return [step, romanToIntegerMap[current]]
 
         case 'C':
             if (next === 'M') return [++step, 900]
             if (next === 'D') return [++step, 400]
-            return [step, 100]
+            return [step, romanToIntegerMap[current]]
 
-        case 'D':
-            return [step, 500]
-
-        case 'M':
-            return [step, 1000]
+        default:
+            return [step, romanToIntegerMap[current]]
     }
 }
 
@@ -36,7 +37,7 @@ function romanToInt(roman: string): number {
     let int: number = 0;
 
     for (let i = 0; i < roman.length;) {
-        const [step, to] = change(roman[i] as Roman, roman[i + 1] as Roman)
+        const [step, to] = change(roman[i] as RomanType, roman[i + 1] as RomanType)
         int += to
         i += step
     }
